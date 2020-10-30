d4493afb4e3bd8553a595fa59037a0ff1e54b957
Ronald Gerssen <ronald@graphicgenie.nl>
2020-10-30 14:22:59 +0100

    install script changes

---
+++ install_root.sh
@@ -0,0 +1,3 @@
+mkdir -p /usr/local/directadmin/scripts/custom
+wget -O /usr/local/directadmin/scripts/custom/ftp_upload.php https://raw.githubusercontent.com/graphicgenie/directadmin-s3-backup/master/upload-script.sh
+chmod +x /usr/local/directadmin/scripts/custom/ftp_upload.php
