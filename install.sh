68e21ea41c4c1ddf28bbc42509bd52e4b1ed3d93
Ronald Gerssen <ronald@graphicgenie.nl>
2020-10-30 14:09:43 +0100

    add install script added ftp_path and day to upload path

--- install.sh
+++ install.sh
@@ -1,16 +1,5 @@
-# directadmin-s3-backup
+#/bin/sh
 
-DirectAdmin S3 Backup
-
-## Features
-
-- Auto backup upload to AWS S3
-- Auto Create S3 Bucket
-- Auto delete old backups after X day(s)
-
-## Installation
-
-```bash
 mkdir -p /home/admin/tools/
 wget -O /home/admin/tools/s3backup.zip https://github.com/graphicgenie/directadmin-s3-backup/archive/master.zip
 cd /home/admin/tools
@@ -26,10 +15,3 @@ mkdir -p /usr/local/directadmin/scripts/custom
 wget -O /usr/local/directadmin/scripts/custom/ftp_upload.php https://raw.githubusercontent.com/graphicgenie/directadmin-s3-backup/master/upload-script.sh
 chmod +x /usr/local/directadmin/scripts/custom/ftp_upload.php
 cp config.sample.php config.php
-```
-
-Update `config.php` with your AWS access keys, region and bucket name
-
-Finally, go to `DirectAdmin \ Admin Backup/Transfer` to create Cron Schedule backup, select FTP for the backup location.
-
-NOTE: The FTP user/pass is your DirectAdmin admin account & password, FTP IP is `127.0.0.1`.
